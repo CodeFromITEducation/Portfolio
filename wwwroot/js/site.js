6f78c3385f430c8fe69bdecb51ceda030833d7f1
CameronITStudent <81937927+CameronITStudent@users.noreply.github.com>
2024-02-23 14:24:10 -0600

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿if (document.querySelector('.bubble')){
+    document.addEventListener("DOMContentLoaded", function () {
+        const canvas = document.querySelector('.canvas');
+        for (let i = 0; i < 50; i++) {
+            const bubble = document.createElement('div');
+            bubble.className = 'bubble';
+            bubble.style.width = `${randomSize()}px`;
+            bubble.style.height = bubble.style.width;
+            bubble.style.left = `${randomPercent()}%`;
+            bubble.style.animationDuration = `${randomDuration()}s`;
+
+            canvas.appendChild(bubble);
+        }
+    });
+
+    function randomSize() {
+        return Math.floor
+            (Math.random() * (100 - 5 + 1)) + 5;
+    }
+
+    function randomPercent() {
+        return Math.floor(Math.random() * 100);
+    }
+
+    function randomDuration() {
+        return Math.floor(Math.random() * (15 - 5 + 1)) + 5;
+    }
+}
